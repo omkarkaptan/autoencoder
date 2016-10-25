7fc4a3a4cd418098e517969f4aa18074a87f8966
Omkar Kaptan <omkarkaptan@gmail.com>
2016-10-24 23:48:49 -0700

    Added prefix test to test names

--- imageutilstest.py
+++ imageutilstest.py
@@ -2,7 +2,7 @@ import unittest
 from imageutils import *
 
 class UtilsTest(unittest.TestCase):
-    def read_single_pgm(self):
+    def test_read_single_pgm(self):
 	pgm_image = open("testresources/sample.pgm", "r")
 	raster = read_pgm_image(pgm_image)
 	self.assertEquals(len(raster), 32)
@@ -14,7 +14,7 @@ class UtilsTest(unittest.TestCase):
 
 	self.assertEquals(total_element_count, 1024)
 
-    def read_pgm_from_directory(self):
+    def test_read_pgm_from_directory(self):
 	directory = "testresources/images"
 	image_count = 0
 	for image in read_pgm_from_directory_generator(directory):
