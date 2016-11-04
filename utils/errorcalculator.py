ce88c580e788c93ff2087e3bc8638980fa5c00c1
Gerard Pinto <gerardfly9@gmail.com>
2016-11-04 00:23:10 -0700

    Changed to for loop

--- utils/errorcalculator.py
+++ utils/errorcalculator.py
@@ -6,7 +6,8 @@ def element_mean_square_error(expectedValues, observedValues):
     return sqrt(mean_squared_error(expectedValues, observedValues))
     
 def batch_mean_square_error(expectedBatchValues, observedBatchSize):
-    batch_mean_square_sum = np.sum(element_mean_square_error(expectedBatchValues, observedBatchSize))
-    return batch_mean_square_sum / len(expectedBatchValues)
-    
-print batch_mean_square_error(np.array([[2,2,3] ,[2,2,3], [2,2,3]]), np.array([[0,2,6], [0,2,6], [0,2,6]]))
+    sum = 0
+    batchSize = len(expectedBatchValues)
+    for index in range(batchSize):
+        sum = sum + element_mean_square_error(expectedBatchValues[index], observedBatchSize[index])
+    return sum / batchSize
